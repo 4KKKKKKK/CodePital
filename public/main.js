73ff8c3f0f3e7b2a248b353c6c772032563e82cf
Nass <nassim.chouirfa@gmail.comgit config user.namegit config --global user.email nassim.chouirfa@gmail.com>
2021-12-09 18:36:04 +0100

    finalisation du projet avec la boucle forEach C.N

--- public/main.js
+++ public/main.js
@@ -1,2 +1,14 @@
 import {docteur,pharmacie,patients} from "./module/instances.js";
 
+patients.forEach(e => {
+    console.log(`Story of ${e.nom}`);
+    console.log(` `);
+    
+    docteur.patientIn(e);
+    docteur.diagnostique(e);
+    e.takeCare(docteur);
+    docteur.patientOut(e);
+    e.goTo(docteur.salleDAttente,pharmacie)
+    pharmacie.vendreTraitement(e);
+    console.log(` `);
+});
